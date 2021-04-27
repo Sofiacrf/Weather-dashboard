42445f6b9152ecae1efd2d73d0b794e64975f234
Sofia Rojas <sofia.rojasflo@gmail.com>
2021-04-27 17:10:33 -0500

    Modified JS that displays the forecast

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ function getCurrentW (city) {
         alert('Error: ' + response.statusText);
       }
 }).then(data => {
-  console.log(data);
-  console.log(data.name, data.main.temp, "Farenheit");
+  // console.log(data);
+  // console.log(data.name, data.main.temp, "Farenheit");
   var long = data.coord.lon;
   var lat = data.coord.lat;
   
@@ -61,32 +61,73 @@ function getCurrentW (city) {
     }
 })
 .then(data => {
-  console.log(data);
+  // console.log(data);
+  var uvIndex = document.createElement("div");
+  uvIndex.innerHTML = data.value;
+
+  displayW.append("UV Index: ", uvIndex);
 })
+
+
+ fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly&appid=${apiId}`)
+ .then(function (response) {
+   if (response.ok) {
+     return (response.json());
+   }
+ })
+ .then(data => {
+   console.log(data, "this is the forecast");
+   console.log(data.list[0].main.temp);
+   console.log(data.list[0].dt_txt);
+   console.log(data.list[0].dt);
+   console.log(data.list[0].main.humidity);
+   console.log(data.list[0].wind);
+  
+  //  To get the icon
+   var icon = data.list[0].weather[0].icon;
+ var iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+ console.log(iconUrl);
+
+ for (var i = 0; i < data.list.length; i++) {
+
+ 
+//  To get the date without the time
+var longDate = data.list[i].dt; 
+var shortDate = longDate * 1000;
+var newDate = new Date(shortDate);
+var newDatestring = newDate.toLocaleDateString();
+console.log(newDatestring);
+
+}
+ })
+ 
 })
 }
 
 
+
+
+
 // Function that get the five days weather forecast
-var getFiveDays = function (location) {
-  console.log(location);
-  var {lat} = location;
-  var {lon} = location;
-  var city = location.name;
-  var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid={28f368d87ec745030ac7ec673fb2056a}`;
+// var getFiveDays = function (location) {
+//   console.log(location);
+//    var {lat} = location;
+//    var {lon} = location;
+
+//    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly&appid={28f368d87ec745030ac7ec673fb2056a}`;
+//    console.log(apiUrl);
   
-    console.log(apiUrl, "five days forecast");
-    fetch(apiUrl)
-    .then(function (response){
-      return response.json();
-    })
-    .then(function (data){
-      showWeather(city, data);
-    })
-    .catch(function(error) {
-      console.log(error);
-    })
-  }
+//     fetch(apiUrl)
+//     .then(function (response){
+//       return response.json();
+//     })
+//     .then(function (data){
+//       console.log(data);
+//     })
+//     .catch(function(error) {
+//       console.log(error);
+//     })
+//   }
     
 
 // On click event listener
